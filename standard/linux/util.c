04a8ba4bb9cc61a01b7dc07362908bf088d3f1c2
dawnlarsson <dawn@dawn.day>
2025-03-22 20:04:28 +0100

    fix broken gitignore

---
+++ standard/linux/util.c
@@ -0,0 +1,400 @@
+#include "../linux.c"
+
+#define MAX_INPUT 1024
+#define MAX_ARGS 64
+
+fn core_basename(string_address buffer)
+{
+        if (buffer == NULL)
+        {
+                print("basename: missing operand\n");
+                return;
+        }
+
+        string_address step = buffer;
+        string_address last_slash = NULL;
+        string_address end = buffer;
+        b8 trailing_slashes = false;
+
+        while (string_get(step))
+        {
+                if (string_get(step) == '/')
+                {
+                        if (!trailing_slashes && string_get(step + 1) != '\0')
+                        {
+                                last_slash = step;
+                        }
+
+                        trailing_slashes = true;
+                }
+                else
+                {
+                        trailing_slashes = false;
+                        end = step + 1;
+                }
+
+                step++;
+        }
+
+        if (end == buffer && last_slash == NULL)
+        {
+                if (buffer[0] == '/')
+                {
+                        print("/\n");
+                        return;
+                }
+        }
+
+        string_address base = last_slash ? last_slash + 1 : buffer;
+
+        positive length = end - base;
+
+        system_call_3(syscall_write, stdout, (positive)base, length);
+        print("\n");
+}
+
+fn core_cat(string_address buffer)
+{
+        if (buffer == NULL)
+        {
+                print("cat: missing operand\n");
+                return;
+        }
+
+        bipolar file_descriptor = system_call_2(syscall_open, (positive)buffer, O_RDONLY);
+
+        if (file_descriptor < 0)
+        {
+                print("cat: Cannot open file: ");
+                print(buffer);
+                print("\n");
+                return;
+        }
+
+        p8 out_buffer[2048];
+        while (1)
+        {
+                bipolar nread = system_call_3(syscall_read, file_descriptor, (positive)out_buffer, 2048);
+                if (nread <= 0)
+                        break;
+
+                system_call_3(syscall_write, 1, (positive)out_buffer, nread);
+        }
+
+        system_call_1(syscall_close, file_descriptor);
+}
+
+fn core_cd(string_address buffer)
+{
+        if (buffer == NULL)
+        {
+                buffer = "/";
+        }
+
+        if (system_call_1(syscall_chdir, (positive)buffer) != 0)
+        {
+                print("cd: No such directory: ");
+                print(buffer);
+                print("\n");
+        }
+}
+
+fn core_clear(string_address buffer)
+{
+        print(TERM_CLEAR_SCREEN);
+}
+
+fn core_chmod(string_address buffer)
+{
+        if (buffer == NULL)
+        {
+                print("chmod: missing operand\n");
+                return;
+        }
+
+        if (system_call_2(syscall_chmod, (positive)buffer, 0777) != 0)
+        {
+                print("chmod: Cannot change permissions: ");
+                print(buffer);
+                print("\n");
+        }
+}
+
+fn core_cp(string_address buffer)
+{
+        if (buffer == NULL)
+        {
+                print("cp: missing operand\n");
+                return;
+        }
+
+        string_address source = buffer;
+        string_address destination = string_first_of(buffer, ' ');
+
+        if (destination == NULL)
+        {
+                print("cp: missing destination\n");
+                return;
+        }
+
+        ADDRESS_TO destination = '\0';
+        destination++;
+
+        bipolar source_fd = system_call_2(syscall_open, (positive)source, O_RDONLY);
+        if (source_fd < 0)
+        {
+                print("cp: Cannot open source file: ");
+                print(source);
+                print("\n");
+                return;
+        }
+
+        bipolar destination_fd = system_call_2(syscall_open, (positive)destination, O_CREAT | O_WRONLY);
+        if (destination_fd < 0)
+        {
+                print("cp: Cannot open destination file: ");
+                print(destination);
+                print("\n");
+                return;
+        }
+
+        p8 out_buffer[1024];
+        while (1)
+        {
+                bipolar nread = system_call_3(syscall_read, source_fd, (positive)out_buffer, 1024);
+                if (nread <= 0)
+                        break;
+
+                system_call_3(syscall_write, destination_fd, (positive)out_buffer, nread);
+        }
+
+        system_call_1(syscall_close, source_fd);
+        system_call_1(syscall_close, destination_fd);
+}
+
+fn core_echo(string_address buffer)
+{
+        if (buffer != NULL)
+                print(buffer);
+
+        print("\n");
+}
+
+fn core_exec(string_address buffer)
+{
+        p8 ADDRESS_TO argv[] = {buffer};
+
+        bipolar result = system_call_2(syscall_execve, (positive)buffer, (positive)argv);
+}
+
+fn core_ls(string_address buffer)
+{
+        const p32 max_line_entries = 8;
+
+        if (buffer == NULL)
+        {
+                buffer = ".";
+        }
+
+        bipolar file_descriptor = system_call_2(syscall_open, (positive)buffer, O_RDONLY | O_DIRECTORY);
+
+        if (file_descriptor < 0)
+        {
+                print("ls: Cannot access '");
+                print(buffer);
+                print("': No such file or directory\n");
+                return;
+        }
+
+        p8 out_buffer[MAX_INPUT];
+        positive entries_count = 0;
+
+        while (1)
+        {
+                bipolar nread = system_call_3(syscall_getdents64, file_descriptor, (positive)out_buffer, MAX_INPUT);
+                if (nread <= 0)
+                        break;
+
+                p8 ADDRESS_TO bpos = out_buffer;
+                while (bpos < out_buffer + nread)
+                {
+                        struct linux_dirent64 ADDRESS_TO d = (struct linux_dirent64 ADDRESS_TO)bpos;
+
+                        if (!(d->d_name[0] == '.' && (d->d_name[1] == '\0' ||
+                                                      (d->d_name[1] == '.' && d->d_name[2] == '\0'))))
+                        {
+                                if (d->d_type == DT_DIR)
+                                {
+                                        print(TERM_BOLD TERM_BLUE);
+                                }
+                                else if (d->d_type == DT_LNK)
+                                {
+                                        print(TERM_CYAN);
+                                }
+                                else if (d->d_type == DT_REG)
+                                {
+                                        print(TERM_RESET);
+                                }
+                                else
+                                {
+                                        print(TERM_YELLOW);
+                                }
+
+                                print(d->d_name);
+                                print(TERM_RESET " ");
+
+                                entries_count++;
+                                if (entries_count % max_line_entries == 0)
+                                        print("\n");
+                        }
+
+                        bpos += d->d_reclen;
+                }
+        }
+
+        if (entries_count % max_line_entries != 0)
+                print("\n");
+
+        system_call_1(syscall_close, file_descriptor);
+}
+
+fn core_pwd(string_address buffer)
+{
+        p8 out_buffer[MAX_INPUT] = {0};
+        system_call_2(syscall_getcwd, (positive)out_buffer, MAX_INPUT);
+        print(out_buffer);
+        print("\n");
+}
+
+fn core_mkdir(string_address buffer)
+{
+        if (buffer == NULL)
+        {
+                print("mkdir: missing operand\n");
+                return;
+        }
+
+        if (system_call_2(syscall_mkdir, (positive)buffer, 0777) != 0)
+        {
+                print("mkdir: Cannot create directory: ");
+                print(buffer);
+                print("\n");
+        }
+}
+
+fn core_mv(string_address buffer)
+{
+        if (buffer == NULL)
+        {
+                print("mv: missing operand\n");
+                return;
+        }
+
+        string_address source = buffer;
+        string_address destination = string_first_of(buffer, ' ');
+
+        if (destination == NULL)
+        {
+                print("mv: missing destination\n");
+                return;
+        }
+
+        ADDRESS_TO destination = '\0';
+        destination++;
+
+        if (system_call_2(syscall_rename, (positive)source, (positive)destination) != 0)
+        {
+                print("mv: Cannot move file: ");
+                print(source);
+                print("\n");
+        }
+}
+
+fn core_mount(string_address buffer)
+{
+        if (buffer == NULL)
+        {
+                print("mount: missing operand\n");
+                return;
+        }
+
+        string_address source = buffer;
+        string_address destination = string_first_of(buffer, ' ');
+
+        if (destination == NULL)
+        {
+                print("mount: missing destination\n");
+                return;
+        }
+
+        ADDRESS_TO destination = '\0';
+        destination++;
+
+        bipolar result = system_call_5(
+            syscall_mount,
+            (positive)source,
+            (positive)destination,
+            (positive)source,
+            MS_BIND,
+            NULL);
+
+        if (result != 0)
+        {
+                print("mount: Cannot mount filesystem: ");
+                print(source);
+                print("\n");
+        }
+}
+
+fn core_exit(string_address buffer)
+{
+        exit(0);
+}
+
+fn core_help(string_address buffer);
+
+typedef fn(ADDRESS_TO core_command_function)(string_address buffer);
+
+typedef struct
+{
+        string_address name;
+        core_command_function function;
+} core_command;
+
+core_command core_commands[] = {
+    {"basename", core_basename},
+    {"cat", core_cat},
+    {"cd", core_cd},
+    {"clear", core_clear},
+    {"cp", core_cp},
+    {"chmod", core_chmod},
+    {"echo", core_echo},
+    {"exec", core_exec},
+    {"exit", core_exit},
+    {"ls", core_ls},
+    {"mkdir", core_mkdir},
+    {"mv", core_mv},
+    {"mount", core_mount},
+    {"pwd", core_pwd},
+    {"help", core_help},
+    {NULL, NULL},
+};
+
+fn core_help(string_address buffer)
+{
+        print("Dawning Shell, WIP, " TERM_RED TERM_BOLD "expect crashes! \n\n" TERM_RESET);
+        print("Available built-in commands:\n");
+
+        core_command ADDRESS_TO command = core_commands;
+
+        while (command->name)
+        {
+                print(" - " TERM_BOLD);
+                print(command->name);
+                print(TERM_RESET "\n");
+
+                command++;
+        }
+
+        print("\n");
+}
