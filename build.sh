6140caa32f5eb8b899806aa1aa916458cfa1a0c7
dawnlarsson <dawn@dawn.day>
2025-03-28 22:54:44 +0100

    try prevent build prompts

--- build.sh
+++ build.sh
@@ -12,14 +12,30 @@ info_name="dawning-linux"
 
 info_full_name="$info_name-$info_version"
 
+sh script/label REPOSITORY SETUP
+
 sudo sh script/setup
+
+
+sh script/label DISTRO INFO
 
 echo "CONFIG_LOCALVERSION=\"$info_full_name\"" > artifacts/info
 echo "CONFIG_DEFAULT_HOSTNAME=\"$info_name-box\"" >> artifacts/info
 
 sudo sh script/build.fs
 
-# Download kernel
+
+sh script/label USER SPACE PROGRAMS
+
+# Compile runtime programs
+sh standard/build_kernel programs/init fs/init
+sh standard/build_kernel programs/shell fs/shell
+sh standard/build_kernel programs/duck fs/duck
+sh standard/build_kernel programs/edit fs/edit
+
+
+sh script/label KERNEL CONFIGURATION
+
 sudo sh script/get.kernel
 
 [ -z "$1" ] || sudo sh script/config any $@
@@ -27,18 +43,13 @@ sudo sh script/get.kernel
 sh script/is_file artifacts/.config || \
         sudo sh script/config any arch/x64 debug_none limbo desktop
 
-# Compile runtime programs
-sh standard/build_kernel programs/init fs/init
-sh standard/build_kernel programs/shell fs/shell
-sh standard/build_kernel programs/duck fs/duck
-
-sh standard/build_kernel programs/edit fs/edit
-
 cd linux
 sudo make allnoconfig
-sh scripts/kconfig/merge_config.sh -m .config ../artifacts/.config
-sudo make olddefconfig
+sudo sh scripts/kconfig/merge_config.sh -n -m .config ../artifacts/.config
+yes n | make oldconfig -n
 cd ..
 
-# Compile kernel
+
+sh script/label KERNEL BUILD
+
 sudo sh script/build.kernel
