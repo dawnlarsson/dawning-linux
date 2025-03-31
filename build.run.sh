55c0e1f9752f57c2b6d722dcc468db0d73f21a19
Dawn Larsson <dawn@dawn.day>
2025-03-31 07:49:46 +0200

    profile drive build.run arch virt target

--- build.run.sh
+++ build.run.sh
@@ -12,4 +12,18 @@
 
 # qemu-system-x86_64 -enable-kvm -cpu host -smp 16 -m 12G -kernel dist/bzImage
 
-qemu-system-x86_64 -smp 6 -m 4G -kernel dist/bootx64.efi
+kernel_image=$(sh script/key kernel_image)
+kernel_export=$(sh script/key kernel_export)
+
+arch=$(sh script/key_single arch)
+
+if [ "$arch" = "x86_64" ]; then
+    qemu-system-x86_64 -enable-kvm -cpu host -smp 4 -m 4G -kernel $kernel_export
+fi
+
+if [ "$arch" = "arm64" ]; then
+    qemu-system-aarch64 -machine virt -cpu cortex-a53 -smp 4 -m 4G -kernel $kernel_export
+fi
+
+
+# qemu-system-x86_64 -smp 6 -m 4G -kernel dist/bootx64.efi
