380904cf72902b0559c2d033490a3f8244db91b0
Dawn Larsson <dawn@dawn.day>
2025-04-07 22:18:24 +0200

    wip

--- src/dawning_core.c
+++ src/dawning_core.c
@@ -63,7 +63,7 @@ fn dawn_init_mount()
                         continue;
                 }
 
-                struct vfsmount ADDRESS_TO mnt = do_add_mount(mnt, &mountpoint_path, mount->filesystem, mount->mount_flags);
+                // struct vfsmount ADDRESS_TO mnt = do_add_mount(mnt, &mountpoint_path, mount->filesystem, mount->mount_flags);
 
                 path_put(ADDRESS_OF mountpoint_path);
                 mount++;
