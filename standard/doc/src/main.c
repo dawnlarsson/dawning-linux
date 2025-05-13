1f557c31128a564820b3524f54e97b2683408910
dawnlarsson <dawn@dawn.day>
2025-05-13 11:21:03 +0200

    linux 6.14.6

--- standard/doc/src/main.c
+++ standard/doc/src/main.c
@@ -1,12 +1,12 @@
-// Dawning Documentation Genorator v1
+// Dawning Documentation Generator v1
 #include "../../library.c"
 #include "../../platform/shell.c"
 
 #define html_style '<link rel="stylesheet" href="https://dawning.dev/runtime/u&i.css">'
-#define tag(name, data, content) string_format(log, "<%s %s>%s</%s>", name, data, content, name)
-#define tag_empty(name, data) string_format(log, "<%s %s/>", name, data)
+#define tag_line(name, data, content) string_format(log, "<%s %s>%s</%s>", name, data, content, name)
+#define tag_empty(name, data) string_format(log, "<%s %s />", name, data)
 
-fn html_push(string_address name, string_address data)
+fn tag(string_address name, string_address data)
 {
     if (!string_get(data))
         return string_format(log, "<%s>", name);
@@ -22,26 +22,17 @@ fn html_pop(string_address name)
 
 b32 main()
 {
-    dawn_shell_cd(log, "..");
+//    dawn_shell_cd(log, "..");
 
-    html_push("!DOCTYPE", "html");
-    html_push("html", "lang=en");
-    html_push("head", "");
+    tag("!DOCTYPE", "html");
+    tag("html", "lang=en");
+    tag("head", "");
     tag_empty("meta", "charset=utf-8");
     tag_empty("meta", "name=viewport content=\"width=device-width initial-scale=1.0\"");
     tag_empty("meta", "name=description content=\"Dawning C Standard Library Documentation\"");
     html_pop("head");
 
-    html_push("body", "");
-
-    file lib = file_new("../library.c", FILE_READ);
-
-    log(str("\n"));
-    log_file(log, lib);
-    log_file_status(log, lib);
-    
-    if(file_load(address_of lib) == null)
-        log_direct(str("Failed to load library.c\n"));
+    tag("body", "");
 
     //log(lib.data, 30);
 
